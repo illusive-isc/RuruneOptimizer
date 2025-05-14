b11789da4fc5cb60d48dfb57ac72574c3e3f71ec
illusice_isc <sheryl.fire.1995@gmail.com>
2025-05-14 22:12:39 +0900

    v1.1.5 MaxParticleを適切に設定するように

--- Runtime/Def/IllRuruneParamDef.cs
+++ Runtime/Def/IllRuruneParamDef.cs
@@ -22,6 +22,7 @@ namespace jp.illusive_isc.RuruneOptimizer
         {
             "HeartGunCollider L",
             "PlayerCollisionHit",
+            "FaceLock",
         };
         private static readonly List<string> NotSyncParameters = new()
         {
@@ -365,11 +366,99 @@ namespace jp.illusive_isc.RuruneOptimizer
             DestroyObj(descriptor.transform.Find("Advanced/Gimmick2/5"));
             DestroyObj(descriptor.transform.Find("Advanced/Gimmick2/6"));
             DestroyObj(descriptor.transform.Find("Advanced/Gimmick2/7"));
-            DestroyObj(descriptor.transform.Find("Advanced/Constraint/Hand_R_Constraint"));
-            DestroyObj(descriptor.transform.Find("Advanced/Constraint/Hand_L_Constraint"));
             DestroyObj(descriptor.transform.Find("Advanced/cameraLight&eyeLookHide"));
 
             return this;
+        }
+
+        public IllRuruneParamDef ParticleOptimize()
+        {
+            SetMaxParticle("Advanced/Particle/1/breath", 100);
+            SetMaxParticle("Advanced/Particle/2/WaterFoot_R/WaterFoot2/WaterFoot3", 10);
+            SetMaxParticle("Advanced/Particle/2/WaterFoot_R/WaterFoot2/WaterFoot3/WaterFoot4", 10);
+            SetMaxParticle("Advanced/Particle/2/WaterFoot_L/WaterFoot2/WaterFoot3", 10);
+            SetMaxParticle("Advanced/Particle/2/WaterFoot_L/WaterFoot2/WaterFoot3/WaterFoot4", 10);
+            SetMaxParticle("Advanced/Particle/3/Headbubbles/bubbles", 50);
+            SetMaxParticle("Advanced/Particle/3/Headbubbles/bubbles/bubbles2", 50);
+            SetMaxParticle("Advanced/Particle/3/Headbubbles/bubbles_Idol", 50);
+            SetMaxParticle("Advanced/Particle/3/Headbubbles/bubbles_Idol/bubbles2", 50);
+            SetMaxParticle("Advanced/Particle/5/8bitheart", 5);
+            SetMaxParticle("Advanced/Particle/5/8bitheart/8bitheart flare", 15);
+
+            SetMaxParticle("Advanced/HeartGunR/HeartGunFLY/HeartGunFLY2", 50);
+            SetMaxParticle("Advanced/HeartGunR/HeartGunFLY/HeartGunFLY2/kira", 3200);
+            SetMaxParticle("Advanced/HeartGunR/HeartGunFLY/HeartGunFLY2/Heart", 100);
+            SetMaxParticle("Advanced/HeartGunR/HeartGunFLY/HeartGunFLY2/shot2", 400);
+            SetMaxParticle("Advanced/HeartGunR/HeartGunFLY/HeartGunFLY2/shot2 (1)", 400);
+            SetMaxParticle("Advanced/HeartGunR/HeartGunFLY/HeartGunChargeStay", 50);
+
+            SetMaxParticle("Advanced/HeartGunR/HeartGunCollider/HeadHit/HeadHit", 50);
+            SetMaxParticle("Advanced/HeartGunR/HeartGunCollider/HeadHit/Heart (1)", 50);
+
+            SetMaxParticle("Advanced/HeartGunL/HeartGunFLY/HeartGunFLY2", 50);
+            SetMaxParticle("Advanced/HeartGunL/HeartGunFLY/HeartGunFLY2/kira", 3200);
+            SetMaxParticle("Advanced/HeartGunL/HeartGunFLY/HeartGunFLY2/Heart", 100);
+            SetMaxParticle("Advanced/HeartGunL/HeartGunFLY/HeartGunFLY2/shot2", 400);
+            SetMaxParticle("Advanced/HeartGunL/HeartGunFLY/HeartGunFLY2/shot2 (1)", 400);
+            SetMaxParticle("Advanced/HeartGunL/HeartGunFLY/HeartGunChargeStay", 50);
+            SetMaxParticle("Advanced/HeartGunL/HeartGunCollider/HeadHit/HeadHit", 50);
+            SetMaxParticle("Advanced/HeartGunL/HeartGunCollider/HeadHit/Heart (1)", 50);
+            SetMaxParticle("Advanced/HeartGunR2/HeartGunFLY/HeartGunFLY2", 50);
+            SetMaxParticle("Advanced/HeartGunR2/HeartGunFLY/HeartGunFLY2/kira", 3200);
+            SetMaxParticle("Advanced/HeartGunR2/HeartGunFLY/HeartGunFLY2/Heart", 100);
+            SetMaxParticle("Advanced/HeartGunR2/HeartGunFLY/HeartGunFLY2/shot2", 400);
+            SetMaxParticle("Advanced/HeartGunR2/HeartGunFLY/HeartGunFLY2/shot2 (1)", 400);
+            SetMaxParticle("Advanced/HeartGunR2/HeartGunFLY/HeartGunChargeStay", 50);
+            SetMaxParticle("Advanced/HeartGunL2/HeartGunFLY/HeartGunFLY2", 50);
+            SetMaxParticle("Advanced/HeartGunL2/HeartGunFLY/HeartGunFLY2/kira", 3200);
+            SetMaxParticle("Advanced/HeartGunL2/HeartGunFLY/HeartGunFLY2/Heart", 100);
+            SetMaxParticle("Advanced/HeartGunL2/HeartGunFLY/HeartGunFLY2/shot2", 1200);
+            SetMaxParticle("Advanced/HeartGunL2/HeartGunFLY/HeartGunFLY2/shot2 (1)", 1200);
+            SetMaxParticle("Advanced/HeartGunL2/HeartGunFLY/HeartGunChargeStay", 50);
+            SetMaxParticle("Advanced/AFK_World/position/swim/Particle System", 10);
+            SetMaxParticle("Advanced/AFK_World/position/IdolParticle", 1);
+            SetMaxParticle("Advanced/AFK_World/position/AFKIN Particle/AFK In0", 1);
+            SetMaxParticle("Advanced/AFK_World/position/AFKIN Particle/AFK In1", 1);
+            SetMaxParticle("Advanced/AFK_World/position/AFKIN Particle/AFK In1/IN S1", 1);
+            SetMaxParticle("Advanced/AFK_World/position/AFKIN Particle/AFK In2", 20);
+            SetMaxParticle("Advanced/AFK_World/position/AFKIN Particle/AFK In2/AFK In (1)", 20);
+            SetMaxParticle("Advanced/AFK_World/position/AFKIN Particle/IN S3", 1);
+            SetMaxParticle("Advanced/AFK_World/position/AFKIN Particle/IN S3/AFK In (1)", 1);
+            SetMaxParticle("Advanced/Pet model/Constraint/Constraint/shark/bubbles_Idol", 10);
+            SetMaxParticle("Advanced/Pet model/Constraint/Constraint/zzz", 1);
+            SetMaxParticle("Advanced/Pet model/Constraint/Constraint/zzz/zzz2", 3);
+            SetMaxParticle("Advanced/Pet model/Constraint/Constraint/zzz/zzz2/zzz3", 3);
+
+            SetMaxParticle("Advanced/butterfly/world/target_constraint/cyou/idolParticle", 20);
+            SetMaxParticle(
+                "Advanced/butterfly/world/target_constraint/cyou/idolParticle/idolParticle2",
+                20
+            );
+            SetMaxParticle(
+                "Advanced/butterfly/world/target_constraint/cyou/idolParticle/idolParticle2/idolParticle3",
+                20
+            );
+            SetMaxParticle("Advanced/butterfly/world/target_constraint/deru", 20);
+            SetMaxParticle("Advanced/butterfly/world/target_constraint/orbit", 20);
+            SetMaxParticle("Advanced/butterfly/world/target_constraint/kabecollision", 1);
+            SetMaxParticle("Advanced/butterfly/IndexHandR/handtap_ONOFF/handtap", 10);
+
+            SetMaxParticle("Advanced/Particle/7/2/pen1_R/PenParticle", 10000);
+            SetMaxParticle("Advanced/Particle/7/2/pen1_R/PenParticle/SubEmitter0", 5000);
+            SetMaxParticle("Advanced/Particle/7/4/pen1_L/PenParticle", 10000);
+            SetMaxParticle("Advanced/Particle/7/4/pen1_L/PenParticle/SubEmitter0", 5000);
+
+            return this;
+        }
+
+        private void SetMaxParticle(string path, int max)
+        {
+            var particleobj = descriptor.transform.Find(path);
+            if (particleobj)
+            {
+                var mainModule = particleobj.GetComponent<ParticleSystem>().main;
+                mainModule.maxParticles = max;
+            }
         }
     }
 }
